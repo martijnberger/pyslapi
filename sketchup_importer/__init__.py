1743eee99670ecf6de297794a628f02bb67dc5d9
Arindam Mondal <arindam.mondal.006@gmail.com>
2021-07-26 16:04:46 +0530

    Bump to 0.23

--- sketchup_importer/__init__.py
+++ sketchup_importer/__init__.py
@@ -38,8 +38,8 @@ from .SKPutil import *
 bl_info = {
     "name": "SketchUp Importer",
     "author": "Martijn Berger, Sanjay Mehta, Arindam Mondal",
-    "version": (0, 22, 'dev_branch'),
-    "blender": (2, 83, 0),
+    "version": (0, 23, 'dev_branch'),
+    "blender": (2, 93, 0),
     "description": "Import of native SketchUp (.skp) files",
     # "warning": "Very early preview",
     "wiki_url": "https://github.com/martijnberger/pyslapi",
